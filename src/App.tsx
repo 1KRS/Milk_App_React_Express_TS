3216f6e7442a6547ac9c94e6fbffaaa9025fd186
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-01-23 16:36:03 +0200

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { db } from './db';
 import './App.css';
+
+  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='app-header'>
+        <h1 className='app-title'>The Milk Store</h1>
+        <h4>Milk you want? Got it we have.</h4>
       </header>
+      <main className='main'>
+        <h1>Main</h1>
+      </main>
+    
     </div>
   );
 }
